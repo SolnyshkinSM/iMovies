817d68a52f4134581ac7da25399757894db4e371
SolnyshkinSM <SolnyshkinSM@gmail.com>
2021-01-20 16:59:46 +0300

    add DidReceiveMemoryWarning

--- Movies/AppDelegate.swift
+++ Movies/AppDelegate.swift
@@ -48,5 +48,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {        
+        cacheImage.removeAllObjects()
+    }
 }
 
